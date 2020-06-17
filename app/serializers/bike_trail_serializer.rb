7969dab7d88da13d726dee65a6ff74f73619116b
github username <github email address>
2020-06-17 13:46:21 -0700

    adds updated messages seed

--- app/serializers/bike_trail_serializer.rb
+++ app/serializers/bike_trail_serializer.rb
@@ -1,11 +1,6 @@
 class BikeTrailSerializer
   include FastJsonapi::ObjectSerializer
-<<<<<<< HEAD
-  attributes :name, :distance, :trail_type
-  # has_many :messages
-=======
   attributes :name, :distance, :trail_type, :locations
->>>>>>> baa0c38efdeeabc83b4f761b64d73139f7bb4c74
   has_many :counters
   has_many :counter_data, through: :counters
   has_many :locations, through: :counters
